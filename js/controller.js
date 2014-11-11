81259ebe37897ff3ffdd74557c54903aef1a1bba
Blade Pan <blade@msn.cn>
2014-11-11 13:35:50 -0500

    fix nav bar, divide page into parts

--- js/controller.js
+++ js/controller.js
@@ -1,51 +1,91 @@
-function HomeCtl($scope, $rootScope, $location) {
-    $scope.currentPath = 'home';
+function MainCtl($scope, $route) {
     $scope.sections = [
         {
-            id : 'home',
+            id : '/',
             title : 'Home'
         },
         {
             id : 'files',
             title : 'Files'
+        },
+        {
+            id : 'timer',
+            title : 'Tomato Timer'
         }
     ];
-    $scope.resume = {
+
+    $scope.sectionClass=function(section){
+        if ($route.current && $route.current.scope && $route.current.scope.id == section.id) {
+            return 'active';
+        }
+        return "";
+    };
+    
+}
+
+function HomeController($scope, $routeParams, Data){
+    $scope.id = '/';
+    $scope.resume = Data.resume;
+}
+
+function FilesController($scope, $routeParams, Data) {
+    $scope.id = 'files';
+    $scope.files = Data.files;
+}
+
+
+function TimerController($scope, $routeParams) {
+    $scope.id = 'timer';
+    
+}
+
+var myApp = angular.module('myApp',['ngRoute']);
+
+myApp.factory('Data', function () {
+    var obj = {};
+    var resume = {
         title : 'Resume',
         url : 'XiaozhongPanResume.pdf'
     };
-    $scope.files = [
-        $scope.resume,
+    obj.resume = resume;
+    obj.files = [
+        resume,
         {
             title : 'Virginia Tech Unofficial Academic Transcript',
             url : 'XiaozhongPanUnofficialAcademicTranscipt.pdf'
         }
     ];
+    return obj;
+});
 
-    var sectionMap = {};
-    for (var i = 0; i < $scope.sections.length; i++) {
-        var section = $scope.sections[i];
-        sectionMap[section.id] = section;
-    };
 
-    $rootScope.$on('$locationChangeSuccess',function(argument) {
-        var path = $location.path();
-        var realPath = 'home';
-        if (path != null && path.length>1) {
-            realPath = path.substring(1);
-        }
-        if (!sectionMap[realPath]) {
-            realPath = 'home';
-        }
-        console.log(realPath);
-        if(realPath != $scope.currentPath){
-            $scope.currentPath = realPath;
-            //maybe scroll top
-        }
-    });
-}
+myApp.controller('MainCtl', MainCtl);
+myApp.controller('HomeController', HomeController);
+myApp.controller('FilesController', FilesController);
+myApp.controller('TimerController', TimerController);
+myApp.config(function($routeProvider, $locationProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'parts/home.html',
+    controller: 'HomeController'
+  })
+  .when('/files', {
+    templateUrl: 'parts/files.html',
+    controller: 'FilesController'
+  })
+  .when('/timer', {
+    templateUrl: 'parts/timer.html',
+    controller: 'TimerController'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
 
-var myApp = angular.module('myApp',[]);
+  // configure html5 to get links working on jsfiddle
+  // $locationProvider.html5Mode({
+  //       enabled: true,
+  //       requireBase: false
+  //   });
+});
 
-myApp.controller('HomeCtl', HomeCtl);
-console.log("define HomeCtl");
+
+console.log("define Controllers");
--- note.md
+++ note.md
@@ -2,3 +2,7 @@
 
 - https://developers.google.com/speed/libraries/devguide
 - http://getbootstrap.com/getting-started/
+
+# nav bar fix
+
+http://getbootstrap.com/examples/navbar-fixed-top/#about
